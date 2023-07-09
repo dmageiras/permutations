d444d2f59c96a7b3b666aa275643b4119d249120
Dimitris Mageiras <dmageiras@gmail.com>
2023-07-09 03:02:59 +0300

    Add project files.

---
+++ permutations/Program.cs
@@ -0,0 +1,28 @@
+﻿string[] permute(string word)
+{
+    List<string> permutations = new List<string>();
+
+    if (word.Length == 1)
+    {
+        permutations.Add(word);
+    }
+    else
+    {
+        for (int i = 0; i < word.Length; i++)
+        {
+            string[] sub_permuations = permute(word.Remove(i, 1));
+            foreach (string permuation in sub_permuations)
+            {
+                permutations.Add(word[i] + permuation);
+            }
+        }
+    }
+    return permutations.ToArray();
+}
+
+string w = "hello world";
+string[] permutations = permute(w);
+foreach (string p in permutations)
+{
+    Console.WriteLine(p);
+}
